a6eded44d4f7a75e859d5bd4b704e098ce40a5de
unknown <kanishka.2019758@iit.ac.lk>
2021-01-26 21:32:09 +0530

    Adding dynamic urls

--- src/main/java/com/satellite_api_master/satellite_monitoring/controller/MonitoringController.java
+++ src/main/java/com/satellite_api_master/satellite_monitoring/controller/MonitoringController.java
@@ -19,15 +19,15 @@ public class MonitoringController {
     @Autowired
     RestTemplate restTemplate;
 
-    @RequestMapping(value = "accessSatellite/{id}")
-    public String accessSatellite(@PathVariable ("id") Long id) {
+    @RequestMapping(value = "accessSatellite/{id}/{url}/{url2}")
+    public String accessSatellite(@PathVariable ("id") Long id, @PathVariable ("url") String url, @PathVariable ("url2") String url2) {
         String satellite_data ="";
         String launch_data ="";
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity <String> entity = new HttpEntity<String>(headers);
-        String satellite_url = String.format("http://localhost:8088/getSatelliteData/%x",id);
-        String launch_url = String.format("http://localhost:8089/getLaunchDataById/%x",id);
+        String satellite_url = String.format("http://%s:8088/getSatelliteData/%x",url,id);
+        String launch_url = String.format("http://%s:8089/getLaunchDataById/%x",url2,id);
         satellite_data = restTemplate.exchange(satellite_url, HttpMethod.GET, entity, String.class).getBody();
         launch_data = restTemplate.exchange(launch_url, HttpMethod.GET, entity, String.class).getBody();
         return(satellite_data + " " + launch_data);
